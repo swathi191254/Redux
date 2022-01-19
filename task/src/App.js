b4a62b7bddcf3c28119c25d9fcc901f37f46db70
Swathi <swathi709357@gmail.com>
2022-01-19 21:14:27 +0530

    commit

---
+++ task/src/App.js
@@ -0,0 +1,44 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  const style = {
+    color: "red",
+    fontSize: "40px",
+    backgroundColor: "blue"
+  };
+  const arr = ["wake-up","have tea", "standup"]
+  
+  return (
+    <div className="App">
+      {[1,2,3,4,5].map((e) => (
+          
+        <h1>Hello {e}</h1>
+      )
+      
+      )}
+   
+      <h1 style={style}> Hello React </h1>
+      <input placeholder="test"/>
+      <div>
+      {arr.map((e) => (
+       <Todos num={e} />
+     ))}
+      </div>
+     <div>
+       <h1 >Hello</h1>
+     </div>
+   
+    </div>
+    
+ 
+  );
+}
+ // interpolation
+
+ function Todos({num}) {
+   
+   return <h1>Todo: {num} </h1>
+ }
+
+export default App;
